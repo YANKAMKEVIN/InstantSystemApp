579c0213170ebbe2a5f1b9318458b410ef86fd07
Kevin Yankam <yankamkevin95@gmail.com>
2025-07-25 13:26:01 +0200

    feat(data): implement di modules

---
+++ app/src/main/java/com/kev/instantsystem/data/di/ApiModule.kt
@@ -0,0 +1,29 @@
+package com.kev.instantsystem.data.di
+
+import com.kev.instantsystem.data.remote.NewsApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApiModule {
+
+    @Provides
+    @Singleton
+    fun provideNewsApi(): NewsApiService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApiService::class.java)
+    }
+
+    companion object {
+        private const val BASE_URL= "https:///newsapi.org/"
+    }
+}
---
+++ app/src/main/java/com/kev/instantsystem/data/dto/SourceDto.kt
@@ -0,0 +1,6 @@
+package com.kev.instantsystem.data.dto
+
+data class SourceDto(
+    val id: String?,
+    val name: String
+)
