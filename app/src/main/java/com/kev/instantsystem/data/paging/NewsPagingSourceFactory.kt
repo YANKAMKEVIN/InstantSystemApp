848b8def36bb8daecb30e0453ab50f117da18efc
Kevin Yankam <yankamkevin95@gmail.com>
2025-07-25 13:23:22 +0200

    feat(data): implement paging source and factory

---
+++ app/src/main/java/com/kev/instantsystem/data/paging/NewsPagingSourceFactory.kt
@@ -0,0 +1,28 @@
+package com.kev.instantsystem.data.paging
+
+import androidx.paging.PagingSource
+import com.kev.instantsystem.data.remote.datasource.NewsRemoteDataSource
+import com.kev.ynfluence.ui.screens.news.ui.model.Article
+import javax.inject.Inject
+
+/**
+ * Factory class to create instances of [NewsPagingSourceFactory].
+ *
+ * Useful to keep construction logic clean and injectable in the repository.
+ */
+open class NewsPagingSourceFactory @Inject constructor(
+    private val remoteDataSource: NewsRemoteDataSource,
+) {
+    open fun create(
+        country: String,
+        category: String?,
+        query: String?
+    ): PagingSource<Int, Article> {
+        return NewsPagingSource(
+            remoteDataSource = remoteDataSource,
+            country = country,
+            category = category,
+            query = query
+        )
+    }
+}
